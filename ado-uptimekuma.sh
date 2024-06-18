84b49a70079856703dad40e7682fff189200b097
kuyajnonymous <91508304+kuyajnonymous@users.noreply.github.com>
2024-06-19 02:18:45 +0800

    Update ado-uptimekuma.sh

--- ado-uptimekuma.sh
+++ ado-uptimekuma.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Update and install necessary packages
-apt-get update
-apt-get install -y curl sudo mc git ca-certificates gnupg
+sudo apt-get update
+sudo apt-get install -y curl sudo mc git ca-certificates gnupg
 
 # Set up NodeSource repository for Node.js
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Clone the Uptime Kuma repository and set it up
-git clone https://github.com/louislam/uptime-kuma.git /opt/uptime-kuma
+sudo git clone https://github.com/louislam/uptime-kuma.git /opt/uptime-kuma
 cd /opt/uptime-kuma
-npm install
-npm run setup
+sudo npm install
+sudo npm run setup
 
 # Create a dedicated user for the service
 sudo useradd -r -m -U -d /opt/uptime-kuma uptime-kuma
 
 # Create a systemd service file for Uptime Kuma
-cat <<EOL > /etc/systemd/system/uptime-kuma.service
+sudo cat <<EOL > /etc/systemd/system/uptime-kuma.service
 [Unit]
 Description=uptime-kuma
 
@@ -34,7 +34,7 @@ WantedBy=multi-user.target
 EOL
 
 # Reload systemd and start the Uptime Kuma service
-systemctl daemon-reload
-systemctl enable --now uptime-kuma.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now uptime-kuma.service
 
 echo "Uptime Kuma installation and setup completed."
